daffc002cb1e087f92c47ef0a1f7f2c5d60a1a1c
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-11-10 21:01:23 +0800

    fix: ensure S3 plugin registers in import map even when conditional

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -101,25 +101,22 @@ export const plugins: Plugin[] = [
     },
   }),
   payloadCloudPlugin(),
-  ...(useS3
-    ? [
-        s3Storage({
-          collections: {
-            media: {
-              prefix: 'media',
-            },
-          },
-          bucket: process.env.S3_BUCKET || '',
-          config: {
-            forcePathStyle: true,
-            credentials: {
-              accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
-              secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
-            },
-            region: process.env.S3_REGION,
-            endpoint: process.env.S3_ENDPOINT,
-          },
-        }),
-      ]
-    : []),
+  s3Storage({
+    enabled: useS3,
+    collections: {
+      media: {
+        prefix: 'media',
+      },
+    },
+    bucket: process.env.S3_BUCKET || '',
+    config: {
+      forcePathStyle: true,
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+      },
+      region: process.env.S3_REGION,
+      endpoint: process.env.S3_ENDPOINT,
+    },
+  }),
 ]
