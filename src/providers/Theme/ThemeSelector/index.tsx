0adf41f8902ebc802875410786e79f36baf3bdf1
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2026-08-08 16:45:47 +0800

    fix(theme): sync selector display with light default on first load

--- src/providers/Theme/ThemeSelector/index.tsx
+++ src/providers/Theme/ThemeSelector/index.tsx
@@ -12,7 +12,7 @@ import React, { useState } from 'react'
 import type { Theme } from './types'
 
 import { useTheme } from '..'
-import { themeLocalStorageKey } from './types'
+import { defaultTheme, themeLocalStorageKey } from './types'
 
 export const ThemeSelector: React.FC = () => {
   const { setTheme } = useTheme()
@@ -30,7 +30,7 @@ export const ThemeSelector: React.FC = () => {
 
   React.useEffect(() => {
     const preference = window.localStorage.getItem(themeLocalStorageKey)
-    setValue(preference ?? 'auto')
+    setValue(preference ?? defaultTheme)
   }, [])
 
   return (
