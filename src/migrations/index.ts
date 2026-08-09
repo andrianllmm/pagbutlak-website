dd60ba53c81050ad9bcfc5a84fccb29e43c37457
romyr <romyrlopez123@gmail.com>
2026-08-10 03:13:45 +0800

    chore: migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,11 +1,13 @@
-import * as migration_20260316_141145_initial from './20260316_141145_initial'
-import * as migration_20260316_141257_rename_posts_to_articles from './20260316_141257_rename_posts_to_articles'
-import * as migration_20260317_131609_add_article_section_field from './20260317_131609_add_article_section_field'
-import * as migration_20260715_024628_add_authors from './20260715_024628_add_authors'
-import * as migration_20260723_051537_add_search_published_at_and_reading_time_fields from './20260723_051537_add_search_published_at_and_reading_time_fields'
-import * as migration_20260723_120033_add_articles_reading_time_field from './20260723_120033_add_articles_reading_time_field'
-import * as migration_20260804_062947_add_media_prefix_field from './20260804_062947_add_media_prefix_field'
-import * as migration_20260807_043333_rework_footer_nav_groups from './20260807_043333_rework_footer_nav_groups'
+import * as migration_20260316_141145_initial from './20260316_141145_initial';
+import * as migration_20260316_141257_rename_posts_to_articles from './20260316_141257_rename_posts_to_articles';
+import * as migration_20260317_131609_add_article_section_field from './20260317_131609_add_article_section_field';
+import * as migration_20260715_024628_add_authors from './20260715_024628_add_authors';
+import * as migration_20260723_051537_add_search_published_at_and_reading_time_fields from './20260723_051537_add_search_published_at_and_reading_time_fields';
+import * as migration_20260723_120033_add_articles_reading_time_field from './20260723_120033_add_articles_reading_time_field';
+import * as migration_20260804_062947_add_media_prefix_field from './20260804_062947_add_media_prefix_field';
+import * as migration_20260807_043333_rework_footer_nav_groups from './20260807_043333_rework_footer_nav_groups';
+import * as migration_20260808_220727_add_new_sections_and_search_section from './20260808_220727_add_new_sections_and_search_section';
+import * as migration_20260809_145546_revert_sections_to_four from './20260809_145546_revert_sections_to_four';
 
 export const migrations = [
   {
@@ -48,4 +50,14 @@ export const migrations = [
     down: migration_20260807_043333_rework_footer_nav_groups.down,
     name: '20260807_043333_rework_footer_nav_groups',
   },
-]
+  {
+    up: migration_20260808_220727_add_new_sections_and_search_section.up,
+    down: migration_20260808_220727_add_new_sections_and_search_section.down,
+    name: '20260808_220727_add_new_sections_and_search_section',
+  },
+  {
+    up: migration_20260809_145546_revert_sections_to_four.up,
+    down: migration_20260809_145546_revert_sections_to_four.down,
+    name: '20260809_145546_revert_sections_to_four'
+  },
+];
