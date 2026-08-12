2bfb25686cf7822e005afc686f4d7e11961454c2
Romyr <romyrlopez123@gmail.com>
2026-08-13 06:23:34 +0800

    Fix migration array syntax in index.ts

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -58,7 +58,6 @@ export const migrations = [
     down: migration_20260810_194922_add_search_section_field.down,
     name: '20260810_194922_add_search_section_field'
   },
-];
     up: migration_20260810_184715_add_multimedia_collection.up,
     down: migration_20260810_184715_add_multimedia_collection.down,
     name: '20260810_184715_add_multimedia_collection',
